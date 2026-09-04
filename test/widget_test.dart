31de12f109944f864200734fea532b93c781b265
FahryAditya <fahryadityagithub@gmail.com>
2026-09-05 06:58:48 +0800

    Aplikasi Ritme-Life-Learning-OS update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,11 +4,10 @@ import 'package:ritme/main.dart';
 
 void main() {
   testWidgets('Ritme smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const RitmeApp());
-    await tester.pumpAndSettle();
+    await tester.runAsync(() async {
+      await tester.pumpWidget(const RitmeApp());
+      await tester.pump(const Duration(milliseconds: 500));
+    });
     expect(find.text('Ritme'), findsOneWidget);
-
-    await tester.pumpWidget(const SizedBox());
-    await tester.pumpAndSettle();
   });
 }
